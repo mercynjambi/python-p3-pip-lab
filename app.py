7e638fce4f8a11c26e87f94e270a67c3122e4dca
mercynjambi <njambim695@gmail.com>
2024-07-10 18:14:25 +0300

    completed assignment

---
+++ app.py
@@ -0,0 +1,33 @@
+
+
+# setting up a virtual environment
+# installing libraries using pipenv
+
+
+# creating a new virtual environment
+
+# pipenv --python 3.8.13
+
+
+# installing specifies dependencies
+# pipenv install requests==2.27.1
+# pipenv install pytest==7.1.3
+
+# pipenv install requests==2.27.1 pytest==7.1.3
+
+
+# activating the virtual environment
+#    pipenv shell
+
+# Running the tests
+# pytest lib/testing_env
+
+
+
+
+
+
+
+
+
+
